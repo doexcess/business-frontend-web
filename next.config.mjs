22401459cddb8d2543b5a0d85a41364fd5a806cf
manuelExtra <hello@mxtechsolutions.com>
2025-11-03 17:48:46 +0100

    implement next config

--- next.config.mjs
+++ next.config.mjs
@@ -25,6 +25,10 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  typescript: {
+    // WARNING: This will allow builds to succeed even with TypeScript errors.
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
